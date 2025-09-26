c0e2596e74d38453aac723b581df14d6b2b1f3ca
Eggy Egzen <eggynandafrayuda@gmail.com>
2025-09-26 19:39:00 +0700

    Percobaan Argumen Default Fungsi

--- src/ArgumenDefaultFungsi.kt
+++ src/ArgumenDefaultFungsi.kt
@@ -3,7 +3,7 @@ fun main(args: Array<String>) {
     connectToDb()
     println()
     //panggil dengan 2 argumen
-    connectToDb("sqlserver", "depandi")
+    connectToDb("sqlserver", "Eggy Nanda Frayuda")
 }
 
 fun connectToDb(hostname: String = "localhost",
